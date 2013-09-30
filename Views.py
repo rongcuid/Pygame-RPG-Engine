cd1b104632decc31e188d4236db04d183897f929
Rongcui Dong <carldong76@gmail.com>
2013-09-30 14:21:21 -0400

    Rewrote ShowMap to overlays

--- Views.py
+++ Views.py
@@ -45,6 +45,8 @@ class PygameView:
         self.window.blit(self.background, (0, 0))
         pygame.display.flip()
 
+        self.backSprite = pygame.sprite.RenderUpdates()
+
     #-------------------------------
 
     def ShowMap(self, gameMap, xoffset=0, yoffset=0):
@@ -57,12 +59,13 @@ class PygameView:
         pygame.display.flip()
 
         # Draw objects
-        for mapLayer in mapLayers:
-            for tile_y in range(0, len(mapLayer)):
-                for tile_x in range(0, len(mapLayer[0])):
-                    self.DrawTile(tile_x, tile_y, mapLayer)
+        map_img = self.DrawTile(mapLayers)
+        overlay = pygame.sprite.Sprite(self.backSprite)
+        overlay.image = map_img
+        overlay.rect = map_img.get_rect()
 
         self.window.blit(self.background, (0, 0))
+        self.backSprite.draw(self.window)
         pygame.display.flip()
         self.state = self.STATE_IDLE
 
@@ -71,14 +74,31 @@ class PygameView:
         # TODO: Overlaid charactor sprite
         sector = charactor.sector
         sprite = charactor.sprite
-        self.background.blit(sprite.image,sprite.rect.topleft)
-        self.window.blit(self.background,(0,0))
+        #self.background.blit(sprite.image,sprite.rect.topleft)
+        #self.window.blit(self.background,(0,0))
         
-    def DrawTile(self, tile_x, tile_y, mapLayer):
-        tile = mapLayer[tile_y][tile_x]
-        if tile:  # Makes sure the tile is not None
-            self.background.blit(
-                tile, (tile_x * GameConstants.TILESIZE, tile_y * GameConstants.TILESIZE))
+    def DrawTile(self, mapLayers):
+        #tile = mapLayer[tile_y][tile_x]
+        #if tile:  # Makes sure the tile is not None
+        #    self.background.blit(
+        #        tile, (tile_x * GameConstants.TILESIZE,
+        #                tile_y * GameConstants.TILESIZE))
+        image = pygame.Surface(self.window.get_size())
+        for mapLayer in mapLayers:
+            for tile_y in range(0, len(mapLayer)):
+                for tile_x in range(0, len(mapLayer[0])):
+                    tile = mapLayer[tile_y][tile_x]
+                    if tile:
+                        self.DrawOneTile(image, tile,
+                                tile_x, tile_y)
+        #self.background.blit(image,(0,0))
+        return image
+    def DrawOneTile(self, image, tile, tile_x, tile_y):
+        if tile:
+            image.blit(tile,
+                    (tile_x * GameConstants.TILESIZE,
+                    tile_y * GameConstants.TILESIZE))
+
 
     #------------------------------
     def Notify(self, event):
@@ -87,8 +107,9 @@ class PygameView:
                 self.frames += 1
                 pygame.display.flip()
         elif isinstance(event, Events.LogicTickEvent):
-            for charactor in event.game.charactors:
-                self.ShowCharactor(charactor)
+            #for charactor in event.game.charactors:
+            #    self.ShowCharactor(charactor)
+            pass
         elif isinstance(event, Events.SecondEvent):
             if GameConstants.SHOW_FPS:
                 Debug("PygameView: FPS = ", self.frames)
